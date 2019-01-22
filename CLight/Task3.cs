2b2716cf63c67833c2e677ddc700ad6e3c50a291
Ярослав Исаев <iyaroslav@yandex.ru>
2019-01-22 15:15:18 +0300

    Task3

---
+++ CLight/Task3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLight
+{
+    class Task3
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 3. Что выведется в консоль?  ВАЖНО!!! Не запускать код и попытаться подумать головой 
+             */
+            int a = 10;
+            int b = 38 * 17;
+            int c = (31 - 5 * a) / b;
+            Console.writeline(c);
+            //Ошибка - у Console нет такого метода как writeline 
+        }
+    }
+}
