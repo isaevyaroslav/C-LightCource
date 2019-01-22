6f5dd3f6e18dd89aafa3bb0b825a7ccb339638c8
Ярослав Исаев <iyaroslav@yandex.ru>
2019-01-22 15:10:40 +0300

    Task2

---
+++ CLight/Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLight
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Объявить 10 переменных разных типов.  Опробовать различные наименования, используя цифры и подчеркивания.  
+             */
+            int a;
+            string A1;
+            bool bool_is_bool;
+            long longLongInt;
+            short shortInt;
+            byte byteIsSimpleAndPositive;
+            sbyte IsNegativeByte;
+            ulong isPositiveLong;
+            char isPartOfWord;
+            ushort positive_short_1;
+
+
+        }
+    }
+}
---
+++ CLight/Task2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLight
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            //Подумать и пометить какие значения будут в следующих переменных: 
+
+            int a = 10;//10
+
+            int b = 15 + 25;//40
+
+            int c = a * b + 8; //408
+
+            int d = (5 + 5) / 2;//5
+
+            bool b_a = true;//true
+
+            bool b_b = true && false;//false
+
+            bool b_c = false || true;//true
+
+            bool b_d = true != false;//true
+
+            bool b_e = b_a == b_c;//true
+
+            string s = "test" + " " + "test2";//test test2
+
+            //ВАЖНО!!! Не запускать код и попытаться подумать головой
+        }
+
+    }
+}
